dcbab69da8b2c48077d1c7c3f03cb3aa5ef83eea
Sergey Medvedev <medvedev.sergeyaa@gmail.com>
2022-07-12 21:14:51 +0500

    Реализовал Increment и Decrement

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import reactDom from 'react-dom';
 import "bootstrap/dist/css/bootstrap.css"
+import App from "./app";
 
-const element = <h1>hello world</h1>
 
-reactDom.render(element, document.getElementById('root'))
+
+reactDom.render(<App />, document.getElementById('root'))
